12002f6a2052fbad309cfca02ad756264d877d71
salome-coder <s.akhaladze.art19@gipa.ge>
2023-02-07 19:01:24 +0400

    initial commit3

---
+++ index.js
@@ -0,0 +1,54 @@
+function typeTexts(id) {
+    if (typeof id === "object" ) {
+
+    } else if (typeof id === "string") {
+        let el = document.getElementById(id);
+        let txt = el.innerText;
+        el.innerHTML = "";
+        el.classList.add('show');
+        new TypeIt(el, {strings: txt}).go();
+    }
+}
+
+/*
+const buttons = document.getElementsByClassName('btn');
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function (event) {
+        //   typewriter(document.getElementById(event.currentTarget.dataset.target));
+        let id = items[event.currentTarget.id].next;
+        new TypeIt("#"+id, {
+            strings: items[id].txt
+        }).go();
+    });
+}
+const imgButtons = document.getElementsByClassName('img-btn');
+for (let i = 0; i < imgButtons.length; i++) {
+    imgButtons[i].addEventListener('click', function (event) {
+        let images = event.currentTarget.parentNode.children;
+        for (let j = 0; j < images.length; j++) {
+            if (images[j] !== event.currentTarget) images[j].classList.remove('show');
+        }
+        event.currentTarget.style.left = "200px";
+        event.currentTarget.style.transitionDelay = "1s";
+    });
+}
+const typewriter = function (el) {
+    let txt = el.innerText;
+    el.innerHTML = '';
+    el.classList.add('show');
+    let i = 0;
+
+    function writeChart() {
+        el.innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(writeChart, 80);
+    };
+    writeChart();
+}
+
+const showSection = function (targetId, sourceId) {
+    document.getElementById(targetId).classList.add('show');
+    document.getElementById(sourceId).classList.remove('show');
+}
+
+ */
